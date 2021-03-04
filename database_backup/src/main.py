f0c229624f2c2dd4eac2889059381c14263a911a
Corbin Murray <corbin.murray63@yahoo.com>
2021-03-03 19:28:39 -0600

    Initial commit

---
+++ database_backup/src/main.py
@@ -0,0 +1,14 @@
+"""
+Description: 
+
+This will be a small program that initializes our x-day/week cache table/database.
+Creates any triggers, functions, etc. that will be used to copy incoming data into our cache.
+"""
+
+def main():
+    """
+    """
+
+
+if __name__ == "__main__":
+    main()
